19d69df128f71fabf375e6d3f5c5a674d1f7b975
madihamallick <mallickmadiha9031@gmail.com>
2023-11-28 17:35:42 +0530

    server setup

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected ${socket.id}`);
+});
+
+server.listen(4000, () => "Server is running on port 3000");
