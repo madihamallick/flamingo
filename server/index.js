74c6f0a0386bbd68eb8864323eb8f1e65a8adecc
madihamallick <mallickmadiha9031@gmail.com>
2023-12-08 17:46:28 +0530

    cors issue fix

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use("/user", userRouter);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000", "https://flamingo-server.onrender.com/"
   },
 });
 
